3290782302c24118ae16bbca79334cfdaefcba53
ospinajuanp <juanpo996@gmail.com>
2022-07-15 01:31:47 -0500

    Tipo de valores en js

---
+++ EscuelaPlatizWeb/BasicoJavaScript/3ElementoDeUnLenguajeDeProgramacion/valores.js
@@ -0,0 +1,17 @@
+// VALORES PRIMITIVOS
+
+40                  // TIPO DE DATO NUMERICO
+
+"HOLA"              // TIPO DE DATO STRING
+
+true                // TIPO DE DATO BOOLEAN
+false               // TIPO DE DATO BOOLEAN
+
+null                // TIPO DE DATO VACIO/PLACEHOLDER
+undefined           // TIPO DE DATO VACIO/PLACEHOLDER
+
+// VALORES NO PRIMITIVOS
+
+[1,2,3]             // TIPO DE DATO ARRAY
+
+{nombre:"juan"}     // TIPO DE DATO OBJETO
