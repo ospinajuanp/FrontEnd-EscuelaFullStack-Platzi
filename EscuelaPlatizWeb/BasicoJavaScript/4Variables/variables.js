8be1acaa9071f5383bc3239c907b3563fd41ed9e
ospinajuanp <juanpo996@gmail.com>
2022-07-15 01:37:03 -0500

    variables en js

---
+++ EscuelaPlatizWeb/BasicoJavaScript/4Variables/variables.js
@@ -0,0 +1,22 @@
+// var palabra reservada para crear variables
+// nombre es como llame mi variable
+// =  es para saber que es asignacion
+// "Juan" es mi informacion guardada
+var nombre = "Juan";
+
+
+// declarar
+var edad;
+
+// inicializar
+edad = 26;
+
+
+var elementos = ["Computadora","Celular"];
+
+var persona = {
+    nombre:"Juan",
+    edad:26
+    }
+
+    
