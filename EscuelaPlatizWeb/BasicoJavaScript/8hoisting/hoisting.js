f99fac7328a922ae9a6293823099134802c16ba2
ospinajuanp <juanpo996@gmail.com>
2022-07-15 02:05:36 -0500

    Hoisting en js

---
+++ EscuelaPlatizWeb/BasicoJavaScript/8hoisting/hoisting.js
@@ -0,0 +1,12 @@
+// llamar una variable antes que se cree
+console.log(miNombre)
+
+var miNombre = "Juan";
+
+
+// llamar una funcion antes que se cree
+hey();
+
+function hey(){
+    console.log(miNombre)
+}
