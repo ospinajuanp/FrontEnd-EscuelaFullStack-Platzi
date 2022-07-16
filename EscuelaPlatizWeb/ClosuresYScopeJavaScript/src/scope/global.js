acd0e04430a88b083f04fda66ec51ced8db9ce3c
ospinajuanp <juanpo996@gmail.com>
2022-07-16 18:59:37 -0500

    variables globales en js

--- EscuelaPlatizWeb/ClosuresYScopeJavaScript/src/scope/global.js
+++ EscuelaPlatizWeb/ClosuresYScopeJavaScript/src/scope/global.js
@@ -6,4 +6,21 @@ var b = 'b'; // declaracion y asignacion
 b = 'bb'; // reasignacion
 var a = 'aa'; // redeclaracion
 
+// global scope
+var fruit = 'Apple'
 
+function bestFruit(){
+    console.log(fruit)
+}
+
+bestFruit()
+
+
+
+function country (){
+    country = 'Colombia' // global
+    console.log(country)
+}
+
+country()
+console.log(country)
