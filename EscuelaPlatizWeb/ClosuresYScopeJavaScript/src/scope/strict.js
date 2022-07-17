53c315bd17b94113124918c05163e65ea414c1b0
ospinajuanp <juanpo996@gmail.com>
2022-07-16 19:49:27 -0500

    Modo estricto en js

---
+++ EscuelaPlatizWeb/ClosuresYScopeJavaScript/src/scope/strict.js
@@ -0,0 +1,14 @@
+// FUNCIONA SIN EL STRICT
+// pi = 3.1416;
+// console.log(pi)
+
+//MODO STRICT ACTIVO
+'use strict'
+let pi = 3.1416;
+console.log(pi)
+
+function myFunction(){
+    pis = 3.1416;
+    console.log(pis)
+}
+myFunction()
