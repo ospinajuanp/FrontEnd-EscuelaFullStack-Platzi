86ef29fda5345056484dc40756553714d110768d
ospinajuanp <juanpo996@gmail.com>
2022-07-16 20:20:57 -0500

    Lexical closure en js

---
+++ EscuelaPlatizWeb/ClosuresYScopeJavaScript/src/closure/lexical.js
@@ -0,0 +1,26 @@
+
+const myGlobal = 0;
+
+function myFunction (){
+    const myNumber = 1;
+    
+    console.log(myGlobal);
+    
+    function parent (){ //funcion interna - closure
+        const inner = 2;
+
+        console.log(myNumber,myGlobal);
+        
+        function child (){  //funcion interna - closure
+            console.log(inner, myNumber, myGlobal);
+            
+        }
+
+        return child();
+
+    }
+
+    return parent();
+}
+
+myFunction()
