6c5da678995b910cba6c272abbba30a1b5dcaccf
ospinajuanp <juanpo996@gmail.com>
2022-07-19 04:26:41 -0500

    correcion de codigo js

--- EscuelaPlatizWeb/PracticoMaquetacionYAnimacionCSS/app.js
+++ EscuelaPlatizWeb/PracticoMaquetacionYAnimacionCSS/app.js
@@ -1,7 +1,25 @@
 const modal = document.querySelector('.modal')
-const button1 = document.querySelector('#button1')
+const modalContent = document.querySelector('.modal__content')
+const openModal = document.querySelectorAll('.openModal')
+const closeModal = document.querySelector('.modal__content--close')
 
-button1.addEventListener('click', () => {
-    modal.classList.remove('hidden')
-    modal.classList.add('visible')
+openModal.forEach((btnModal) =>
+    btnModal.addEventListener('click', () => {
+        openModalFunction(modal)
+        openModalFunction(modalContent)
+    }) 
+)
+
+closeModal.addEventListener('click', () => {
+    closeModalFunction(modal)
+    closeModalFunction(modalContent)
 })
+
+const openModalFunction = function  (objModal){
+    objModal.classList.remove('hidden')
+    objModal.classList.add('visible')
+}
+const closeModalFunction = function  (objModal){
+    objModal.classList.remove('visible')
+    objModal.classList.add('hidden')
+}
