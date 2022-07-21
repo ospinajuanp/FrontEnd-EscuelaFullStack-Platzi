e260d2b1a28d18804ab4ca98ac19fbdcab3095cc
ospinajuanp <juanpo996@gmail.com>
2022-07-21 01:59:35 -0500

    Combinando la pag principal con 13-carrito-compra y solucionando los toggle

--- EscuelaPlatizWeb/PracticoBasicoJavaScrip/16PracticoFrontendDeveloper/main.js
+++ EscuelaPlatizWeb/PracticoBasicoJavaScrip/16PracticoFrontendDeveloper/main.js
@@ -2,19 +2,40 @@ const menuEmail =  document.querySelector('.navbar-email');
 const desktopMenu  =  document.querySelector('.desktop-menu');
 const menuHam  =  document.querySelector('.menu');
 const mobileMenu  =  document.querySelector('.mobile-menu');
+const menuCarrito  =  document.querySelector('.navbar-shooping-cart');
+const aside  =  document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
+menuHam.addEventListener('click',toggleMobileMenu);
+menuCarrito.addEventListener('click',toggleCarritoAside);
+
 
 
 function toggleDesktopMenu (){
     // desktopMenu.classList.contains('inactive') ? desktopMenu.classList.remove('inactive') : desktopMenu.classList.add('inactive'); //esta fue la forma como yo lo hice que era un reto
+    const isAsideOpen = aside.classList.contains('inactive');
+    if(!isAsideOpen){
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive')
 }
-
-menuHam.addEventListener('click',toggleMobileMenu);
-
-
 function toggleMobileMenu (){
+    const isAsideOpen = aside.classList.contains('inactive');
+    if(!isAsideOpen){
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive')
+    
+}
+function toggleCarritoAside (){
+    const isDesktopMenuOpen = desktopMenu.classList.contains('inactive');
+    const isMobileMenuOpen = mobileMenu.classList.contains('inactive');
+    if(!isDesktopMenuOpen){
+        desktopMenu.classList.add('inactive');
+    } 
+    if(!isMobileMenuOpen){
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive')
 }
