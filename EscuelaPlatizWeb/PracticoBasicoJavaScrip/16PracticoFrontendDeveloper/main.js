8f72df1681bbc0bfcdb98b444aa8dd1e22763525
ospinajuanp <juanpo996@gmail.com>
2022-07-21 00:57:59 -0500

    Combinando la pag principal con 8-Menu-Mobile y arreglando los media query

--- EscuelaPlatizWeb/PracticoBasicoJavaScrip/16PracticoFrontendDeveloper/main.js
+++ EscuelaPlatizWeb/PracticoBasicoJavaScrip/16PracticoFrontendDeveloper/main.js
@@ -1,10 +1,20 @@
 const menuEmail =  document.querySelector('.navbar-email');
 const desktopMenu  =  document.querySelector('.desktop-menu');
+const menuHam  =  document.querySelector('.menu');
+const mobileMenu  =  document.querySelector('.mobile-menu');
 
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 
 
 function toggleDesktopMenu (){
-    desktopMenu.classList.contains('inactive') ? desktopMenu.classList.remove('inactive') : desktopMenu.classList.add('inactive'); //
+    // desktopMenu.classList.contains('inactive') ? desktopMenu.classList.remove('inactive') : desktopMenu.classList.add('inactive'); //esta fue la forma como yo lo hice que era un reto
+    desktopMenu.classList.toggle('inactive')
+}
+
+menuHam.addEventListener('click',toggleMobileMenu);
+
+
+function toggleMobileMenu (){
+    mobileMenu.classList.toggle('inactive')
 }
