f6a8be8be2c787f443feff6c40aa0389e4d8ad0a
ospinajuanp <juanpo996@gmail.com>
2022-07-25 18:46:49 -0500

    Generando codigo clase y reto 3 map-reload

---
+++ EscuelaPlatizWeb/ManipulacionArrays/FrontBack-End/server/03-map-reload.js
@@ -0,0 +1,40 @@
+const orders = [
+    {
+        customerName:'Nicolas',
+        total:60,
+        delivered:true,
+    },
+    {
+        customerName:'Zulema',
+        total:120,
+        delivered:false
+    },
+    {
+        customerName:'Santiago',
+        total:180,
+        delivered:true
+    },
+    {
+        customerName:'Valentina',
+        total:240,
+        delivered:true
+    }
+]
+// console.log(orders);
+// const rta = orders.map(item => item.total)
+// console.log(rta);
+
+
+// const rta2 = orders.map(item => item.tax = .19)
+// console.log(orders);
+// console.log(rta2);
+
+
+const rta3 = orders.map(item =>{ 
+    return { 
+        ...item,
+        tax: .19
+    }
+})
+console.log(orders);
+console.log(rta3);
