953f582dab4384e67e41588a1e93cca8b7b0f8b4
ospinajuanp <juanpo996@gmail.com>
2022-07-30 23:50:47 -0500

    Agrengando img fav

--- Desafios/17TheDogApi/src/js/API.mjs
+++ Desafios/17TheDogApi/src/js/API.mjs
@@ -1,7 +1,9 @@
 export function APIKEY (){
     return '?api_key=7cb9ae40-d8a1-4dfe-95da-722e22fd36bf';
+    // return '?api_key=07a8f273-ac83-4c76-a382-061ffdcdb715';
 }
 
 export function APIURL (){
     return 'https://api.thedogapi.com/v1';
+    // return 'https://api.thecatapi.com/v1';
 }
--- Desafios/17TheDogApi/src/js/main.mjs
+++ Desafios/17TheDogApi/src/js/main.mjs
@@ -1,5 +1,6 @@
 import { APIKEY,APIURL } from './API.mjs'
 const container = document.getElementById('container-content')
+const containerFav = document.getElementById('container-content-fav')
 
 
 // API KEY
@@ -14,40 +15,97 @@ const EP_IMG_FAVOURITES = '/favourites';
 const numberMaxReturn = 5;
 const maxReturn = `&limit=${numberMaxReturn}`;
 const formatReturn = `&mime_types=jpg`
+// const favImg = `&image_id=`
 
 // URLS
 const urlRandomDogs = `${API_URL}${EP_IMG_RANDOM}${API_KEY}${formatReturn}${maxReturn}`;
+let urlFavImg = `${API_URL}${EP_IMG_FAVOURITES}${API_KEY}`;
+let urlSaveFavImg = `${API_URL}${EP_IMG_FAVOURITES}${API_KEY}`;
 
 const getData = async (urlData) => { 
     const response = (await fetch(urlData)).json();
     return response;
 }
 
+const saveDogFav = async (item) => {
+    let id = item.target.id
+    let rest = await fetch(urlSaveFavImg,{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body:JSON.stringify({
+            image_id:`${id}`
+        }
+        )
+    }
+    );
+    const data = await rest.json();
+}
 
+let idsDogs = []
+
+const addEventClick = () => { 
+idsDogs.forEach(item => {
+    let btnEvent = document.getElementById(`${item}`)
+    btnEvent.addEventListener('click', saveDogFav.bind(item))
+})
+}
+
+const renderDogs = (dogsData,card) => { 
+    for (let index = 0; index < dogsData.length; index++) {
+        let view = `
+            <div class="${card}">
+                <div class="card-dog__raza">${dogsData[index]?.breeds[0]?.name || 'Unknow'}</div>
+                <div class="card-dog__img"  style="background:url('${dogsData[index].url}')">
+                    <img src="">
+                </div>
+                <!-- <div class="card-dog__characteristics">
+                    <div>width</div>
+                    <div>height</div>
+                </div> -->
+                <div class="card-dog__fav" id="${dogsData[index].id}">💘 Fav</div>
+            </div>
+            `
+            container.innerHTML+=view;                
+        }
+}
+const renderDogsFavorites = (dogsData,card) => { 
+    for (let index = 0; index < dogsData.length; index++) {
+        let view = `
+            <div class="${card}">
+                <div class="card-dog__img"  style="background:url('${dogsData[index].image.url}')">
+                    <img src="">
+                </div>
+                <!-- <div class="card-dog__characteristics">
+                    <div>width</div>
+                    <div>height</div>
+                </div> -->
+                <div class="card-dog__fav">💘 Fav</div>
+            </div>
+            `
+            containerFav.innerHTML+=view;                
+        }
+}
 
 const insertDog = async () => { 
     try {
-        let dogsData = await getData(urlRandomDogs);
-        for (let index = 0; index < dogsData.length; index++) {
-            let view = `
-                <div class="card-dog">
-                    <div class="card-dog__raza">${dogsData[index]?.breeds[0]?.name || 'Unknow'}</div>
-                    <div class="card-dog__img"  style="background:url('${dogsData[index].url}')">
-                        <img src="">
-                    </div>
-                    <div class="card-dog__characteristics">
-                        <div>width</div>
-                        <div>height</div>
-                    </div>
-                    <div class="card-dog__fav" id="${dogsData[index].id}" >💘 Fav</div>
-                </div>
-                `
-            container.innerHTML+=view;
-        }
+        let dogsDataRandom = await getData(urlRandomDogs);
+        renderDogs(dogsDataRandom,'card-dog__random');
+        dogsDataRandom.forEach(item => {
+            let idDog = item.id
+            idsDogs.push(`${idDog}`)
+        })
+        addEventClick()
+        let dogsDataFavorites = await getData(urlFavImg);
+        console.log(dogsDataFavorites);
+        renderDogsFavorites(dogsDataFavorites,'card-dog__favorites');
+        
     } catch (error) {
         console.error(error)
     }
-    
 }
+
+
 
 insertDog()
