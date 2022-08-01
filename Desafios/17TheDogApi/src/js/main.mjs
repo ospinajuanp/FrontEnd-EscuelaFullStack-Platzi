3664c3f1360b61c9cf5a5f2a8d80f6afa245c32c
ospinajuanp <juanpo996@gmail.com>
2022-08-01 16:55:07 -0500

    Agregando comentarior y funcionalidad de eliminar de fav

--- Desafios/17TheDogApi/src/js/main.mjs
+++ Desafios/17TheDogApi/src/js/main.mjs
@@ -2,6 +2,9 @@ import { APIKEY,APIURL } from './API.mjs'
 const container = document.getElementById('container-content')
 const containerFav = document.getElementById('container-content-fav')
 const faileLoad = document.getElementById('faile-load-page')
+const reloadRandom = document.getElementById('reloadRandom')
+
+
 
 // API KEY
 const API_KEY = APIKEY();
@@ -21,6 +24,7 @@ const formatReturn = `&mime_types=jpg`
 const urlRandomDogs = `${API_URL}${EP_IMG_RANDOM}${API_KEY}${formatReturn}${maxReturn}`;
 let urlFavImg = `${API_URL}${EP_IMG_FAVOURITES}${API_KEY}`;
 let urlSaveFavImg = `${API_URL}${EP_IMG_FAVOURITES}${API_KEY}`;
+let urlDeleteFavImg = (id) => `${API_URL}${EP_IMG_FAVOURITES}/${id}/${API_KEY}`;
 
 // GET -  traer datos de la URL
 const getData = async (urlData) => { 
@@ -30,9 +34,10 @@ const getData = async (urlData) => {
 
 // renderizar perros aleatorios
 const renderDogs = (dogsData,card) => { 
+    container.innerHTML=''
     for (let index = 0; index < dogsData.length; index++) {
         let view = `
-        <div class="${card}">
+        <div class="card-dog__random">
         <div class="card-dog__raza">${dogsData[index]?.breeds[0]?.name || 'Unknow'}</div>
         <div class="card-dog__img"  style="background:url('${dogsData[index].url}')">
         <img src="">
@@ -41,7 +46,7 @@ const renderDogs = (dogsData,card) => {
             <div>width</div>
             <div>height</div>
         </div> -->
-        <div class="card-dog__fav" id="${dogsData[index].id}">💘 Fav</div>
+        <div class="card-dog__fav" id="${dogsData[index].id}">💘 Add Favorite</div>
         </div>
         `
         container.innerHTML+=view;                
@@ -54,7 +59,7 @@ const renderDogsFavorites = (dogsData) => {
         let view = `
         <div class="card-dog__favorites">
             <div class="card-dog__img"  style="background:url('${dogsData[index].image.url}')"></div>
-            <div class="card-dog__fav">💘 Info</div>
+            <div class="card-dog__fav" id="${dogsData[index].id}">💔 Delete Favorite</div>
         </div>
         `
         containerFav.innerHTML+=view;
@@ -63,12 +68,20 @@ const renderDogsFavorites = (dogsData) => {
 
 // array para almacenar los id de los perros generados
 let idsDogs = []
+let idsDogsFav = []
 
-// agregando evento click a los favoritos
-const addEventClick = () => { 
+// agregando evento click a los add favoritos
+const addEventClickFav = () => { 
     idsDogs.forEach(item => {
         let btnEvent = document.getElementById(`${item}`)
         btnEvent.addEventListener('click', saveDogFav.bind(item))
+    })
+}
+// agregando evento click a los delete favoritos
+const addEventClickDele = () => { 
+    idsDogsFav.forEach(item => {
+        let btnEvent = document.getElementById(`${item}`)
+        btnEvent.addEventListener('click', deleteDogFav.bind(item))
     })
 }
 
@@ -91,21 +104,61 @@ const saveDogFav = async (item) => {
     }else{
         let dogsDataFavorites = await getData(urlFavImg);
         renderDogsFavorites(dogsDataFavorites);
+        idsDogs = []
+        dogsDataRandom.forEach(item => {
+            let idDog = item.id
+            idsDogs.push(`${idDog}`)
+        })
+        addEventClickFav()
+    }
+}
+
+// eliminar perro a mi lista de favoritos
+const deleteDogFav = async (item) => {
+    let id = item.target.id
+    let rest = await fetch(urlDeleteFavImg(id),{
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    // validacion de error
+    if (rest.status != 200){
+        faileLoad.innerText='Fallo con el servidor, volver a intentar(F5) en unos minutos'
+    }else{
+        let dogsDataFavorites = await getData(urlFavImg);
+        renderDogsFavorites(dogsDataFavorites);
+        idsDogsFav = []
+        dogsDataFavorites.forEach(item => {
+            let idDog = item.id
+            idsDogsFav.push(`${idDog}`)
+        })
+        addEventClickDele()
     }
 }
 
 // render inicial - datos de perros aleatorios - datos lista de perros favoritos - 
 const insertDog = async () => { 
     try {
+        // TRAER DATA RANDOM
         let dogsDataRandom = await getData(urlRandomDogs);
-        renderDogs(dogsDataRandom,'card-dog__random');
+        renderDogs(dogsDataRandom);
+        idsDogs = []
         dogsDataRandom.forEach(item => {
             let idDog = item.id
             idsDogs.push(`${idDog}`)
         })
-        addEventClick()
+        addEventClickFav()
+        
+        // TRAER DATA FAVORITE
         let dogsDataFavorites = await getData(urlFavImg);
         renderDogsFavorites(dogsDataFavorites);
+        idsDogsFav = []
+        dogsDataFavorites.forEach(item => {
+            let idDog = item.id
+            idsDogsFav.push(`${idDog}`)
+        })
+        addEventClickDele()
     } catch (error) {
         console.error(error)
     }
@@ -113,3 +166,6 @@ const insertDog = async () => {
 
 // ejecucion inicial de la pag
 insertDog()
+
+// boton de reload random dogs
+reloadRandom.addEventListener('click',insertDog)
