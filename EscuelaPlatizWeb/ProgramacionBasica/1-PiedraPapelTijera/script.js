3672a434455dec10996bda01e09192216712afb9
Ospinajuanp <juanpo996@gmail.com>
2023-01-02 22:48:52 -0500

    Agregando logica de ganador y perdedor

--- EscuelaPlatizWeb/ProgramacionBasica/1-PiedraPapelTijera/script.js
+++ EscuelaPlatizWeb/ProgramacionBasica/1-PiedraPapelTijera/script.js
@@ -1,17 +1,57 @@
-let jugador, pc;
+let playerOption, machineOption;
 
 alert('Bienvenido al juego de piedra papel o tijera')
 
-function escoje() {
-    jugador = prompt(`Escoja una opcion
-    1.Piedra, 2.Papel 3.Tijera, 4.Salir`);
-    
-    if((jugador<1) || (jugador>3) || (jugador==null)){
-        alert('Ingreso errado')
-        escoje()
+function getRandomNumber(num = 3){
+    return Math.floor(Math.random() * num);
+}
+function getOptionSelect (numberOption){
+    switch(numberOption){
+        case 1:
+            return 'Piedra'
+            break;
+        case 2:
+            return 'Papel'
+            break;
+        case 3:
+            return 'Tijera'
+            break;
+        default:
+            console.error(`Eleccion invalidad`)
+            break;
     }
 }
-escoje()
+
+function playGame() {
+    playerOption = prompt(`Escoja una opcion
+    1.Piedra, 2.Papel 3.Tijera, 4.Salir`);
+    
+    playerOption = parseInt(playerOption)
+
+    machineOption = parseInt(getRandomNumber())
+
+    if(playerOption==4){
+        return
+    }
+    alert(`${playerOption} - ${machineOption}`)
+
+    if((playerOption<1) || (playerOption>3) || (playerOption==null)){
+        alert('Ingreso errado')
+        playGame()
+    }
+    if(playerOption == machineOption){
+        alert(`Empate Jugador Eligion ${getOptionSelect(playerOption)} y la maquina Eligio ${getOptionSelect(machineOption)}`)
+        playGame()
+    }
+    if((playerOption == 1 && machineOption == 2) || (playerOption == 2 && machineOption == 3 ) || (playerOption == 3 && machineOption == 1)){
+        alert(`Perdio Jugador Eligion ${getOptionSelect(playerOption)} y la maquina Eligio ${getOptionSelect(machineOption)}`)
+        playGame()
+    }
+    if((playerOption == 1 && machineOption == 3) || (playerOption == 2 && machineOption == 3) || (playerOption == 3 && machineOption == 2)){
+        alert(`Gano Jugador Eligion ${getOptionSelect(playerOption)} y la maquina Eligio ${getOptionSelect(machineOption)}`)
+        playGame()
+    }
 
 
-alert('Eligio '+jugador)
+}
+playGame()
