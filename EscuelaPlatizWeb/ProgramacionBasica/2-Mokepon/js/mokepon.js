d8a92fcd9b9dc32e4b0fb1319e3f2c0893ee8468
Ospinajuanp <juanpo996@gmail.com>
2023-01-05 10:58:22 -0500

    agregando la funcionalidad manipulacion DOM la pet seleccionada por el jugador se pongan en la section de vidas

--- EscuelaPlatizWeb/ProgramacionBasica/2-Mokepon/js/mokepon.js
+++ EscuelaPlatizWeb/ProgramacionBasica/2-Mokepon/js/mokepon.js
@@ -1,7 +1,7 @@
-function checkSelectPet (petsSelect){
-    for (pet of petsSelect){
-        if(pet.petSelect.checked){
-            alert(`You selected the ${pet.pet}`)
+function checkSelectPet (petsSelect,spanSetPet){
+    for (pets of petsSelect){
+        if(pets.petSelect.checked){
+            spanSetPet.innerHTML = pets.pet
             return
         }
     }
@@ -10,6 +10,8 @@ function checkSelectPet (petsSelect){
 
 function selectPetPlayer(){
     let inputPets = []
+    let spanPetPlayer = document.getElementById('pet-Player')
+    let spanPetEnemy = document.getElementById('pet-Enemy')
     inputPets.push({petSelect:document.getElementById('hipodoge'),pet:'hipodoge'})
     inputPets.push({petSelect:document.getElementById('capipepo'),pet:'capipepo'})
     inputPets.push({petSelect:document.getElementById('ratigueya'),pet:'ratigueya'})
@@ -17,7 +19,10 @@ function selectPetPlayer(){
     inputPets.push({petSelect:document.getElementById('tucapalma'),pet:'tucapalma'})
     inputPets.push({petSelect:document.getElementById('pudos'),pet:'pudos'})
     
-    checkSelectPet(inputPets)
+    checkSelectPet(inputPets,spanPetPlayer)
+    
+    
+
 }      
 
 function startGame(){
