4104237929e084a7d37eb058e57f6f92820a4ef8
Ospinajuanp <juanpo996@gmail.com>
2023-01-05 11:11:57 -0500

    agregando funcion de ramdom numero y funcion para eleccion de la del del enemigo

--- EscuelaPlatizWeb/ProgramacionBasica/2-Mokepon/js/mokepon.js
+++ EscuelaPlatizWeb/ProgramacionBasica/2-Mokepon/js/mokepon.js
@@ -1,29 +1,40 @@
-function checkSelectPet (petsSelect,spanSetPet){
+let inputPets = [] 
+
+
+function getRandomNumber(num = 6){
+    return Math.floor((Math.random() * num) + 1);
+}
+function insertPet (spanSelect,pet){
+    spanSelect.innerHTML = pet
+}
+
+function selectPetEnemy(){
+    let spanPetEnemy = document.getElementById('pet-Enemy')
+    let pet = inputPets[getRandomNumber()].pet.toString()
+    insertPet(spanPetEnemy,pet)
+    
+}
+
+function checkSelectPet (petsSelect){
+    let spanPetPlayer = document.getElementById('pet-Player')
     for (pets of petsSelect){
         if(pets.petSelect.checked){
-            spanSetPet.innerHTML = pets.pet
+            insertPet(spanPetPlayer,pets.pet)
+            selectPetEnemy()
             return
         }
     }
     alert('YOU HAVE NOT SELECTED ANY MOKEPON')
 }
-
 function selectPetPlayer(){
-    let inputPets = []
-    let spanPetPlayer = document.getElementById('pet-Player')
-    let spanPetEnemy = document.getElementById('pet-Enemy')
     inputPets.push({petSelect:document.getElementById('hipodoge'),pet:'hipodoge'})
     inputPets.push({petSelect:document.getElementById('capipepo'),pet:'capipepo'})
     inputPets.push({petSelect:document.getElementById('ratigueya'),pet:'ratigueya'})
     inputPets.push({petSelect:document.getElementById('langostelvis'),pet:'langostelvis'})
     inputPets.push({petSelect:document.getElementById('tucapalma'),pet:'tucapalma'})
     inputPets.push({petSelect:document.getElementById('pudos'),pet:'pudos'})
-    
-    checkSelectPet(inputPets,spanPetPlayer)
-    
-    
-
-}      
+    checkSelectPet(inputPets)
+}
 
 function startGame(){
     let buttonPetPlayer = document.getElementById('button-select_pet')
