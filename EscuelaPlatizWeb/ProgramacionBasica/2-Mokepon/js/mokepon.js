8d4a3ab1013aceab9893c83ee490e72b4feb3177
Ospinajuanp <juanpo996@gmail.com>
2023-01-05 10:48:07 -0500

    Funcion selectpetplayer y checkselecpet las cuales capturarn los input y verifican si fue y cual fue selecionada de las opciones de seleccion de pet

--- EscuelaPlatizWeb/ProgramacionBasica/2-Mokepon/js/mokepon.js
+++ EscuelaPlatizWeb/ProgramacionBasica/2-Mokepon/js/mokepon.js
@@ -1,6 +1,24 @@
-function selectPetPlayer(){
-
+function checkSelectPet (petsSelect){
+    for (pet of petsSelect){
+        if(pet.petSelect.checked){
+            alert(`You selected the ${pet.pet}`)
+            return
+        }
+    }
+    alert('YOU HAVE NOT SELECTED ANY MOKEPON')
 }
+
+function selectPetPlayer(){
+    let inputPets = []
+    inputPets.push({petSelect:document.getElementById('hipodoge'),pet:'hipodoge'})
+    inputPets.push({petSelect:document.getElementById('capipepo'),pet:'capipepo'})
+    inputPets.push({petSelect:document.getElementById('ratigueya'),pet:'ratigueya'})
+    inputPets.push({petSelect:document.getElementById('langostelvis'),pet:'langostelvis'})
+    inputPets.push({petSelect:document.getElementById('tucapalma'),pet:'tucapalma'})
+    inputPets.push({petSelect:document.getElementById('pudos'),pet:'pudos'})
+    
+    checkSelectPet(inputPets)
+}      
 
 function startGame(){
     let buttonPetPlayer = document.getElementById('button-select_pet')
