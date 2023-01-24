e65cb873bf6e6dfeda7b2fe61e2abeb7e848185d
Ospinajuanp <juanpo996@gmail.com>
2023-01-24 01:22:51 -0500

    includes para los arrays

---
+++ EscuelaPlatizWeb/ECMAScriptHistoriaYVersionesjs/src/es7/01-array-includes.js
@@ -0,0 +1,10 @@
+let numbers = [0,1,1,2,3,5,8,13,21]
+
+console.log(numbers.includes(5));
+console.log(numbers.includes(7));
+
+
+let numbers2 = ['Juan','Pablo']
+
+console.log(numbers2.includes('Juan'));
+console.log(numbers2.includes('Ospina'));
