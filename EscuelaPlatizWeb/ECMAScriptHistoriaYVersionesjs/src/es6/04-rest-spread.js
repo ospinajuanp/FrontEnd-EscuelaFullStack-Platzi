ca741c88088f7b9194ffe3a0b026ae278426c1f9
Ospinajuanp <juanpo996@gmail.com>
2023-01-23 23:37:06 -0500

    Destructuring

---
+++ EscuelaPlatizWeb/ECMAScriptHistoriaYVersionesjs/src/es6/04-rest-spread.js
@@ -0,0 +1,9 @@
+// arrays destructuring
+let fruits = ['Apple', 'Banana'];
+let [a, b] = fruits;
+console.log(a,fruits[1]);
+
+// object destructuring 
+let user = {username:'juan',age:26}
+let {username,age} = user
+console.log(username, user.age);
