d65f6cf5e3302b040f718ee73f05a1e9ca26afda
Ospinajuanp <juanpo996@gmail.com>
2023-01-24 10:26:07 -0500

    This global en todo lado

---
+++ EscuelaPlatizWeb/ECMAScriptHistoriaYVersionesjs/src/es11/04-global-this.js
@@ -0,0 +1,4 @@
+// console.log(window); // Este es el this global en el navegador
+console.log(global); // Este es el this global en el node
+console.log(self); // Este es el this global en el webworker
+console.log(globalThis); // Este es el this global en el todos
