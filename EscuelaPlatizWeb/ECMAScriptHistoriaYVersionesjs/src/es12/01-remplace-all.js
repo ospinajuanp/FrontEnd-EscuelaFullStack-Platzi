44285fb469c554aaf550e0ea9766c317c3b61465
Ospinajuanp <juanpo996@gmail.com>
2023-01-24 10:54:44 -0500

    Remplace all

---
+++ EscuelaPlatizWeb/ECMAScriptHistoriaYVersionesjs/src/es12/01-remplace-all.js
@@ -0,0 +1,6 @@
+const string = 'Javascript es un maravilloso lenguaje de programacion';
+
+const replacedString = string.replace('Javascript','Typescript')
+
+console.log(string);
+console.log(replacedString);
