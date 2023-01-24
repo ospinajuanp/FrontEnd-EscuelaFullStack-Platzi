825afa88356fb64cd784c62c2f374962faf4815f
Ospinajuanp <juanpo996@gmail.com>
2023-01-24 01:42:15 -0500

    Completando un string con un x caracteres para completar un max de caracteres totales totales

---
+++ EscuelaPlatizWeb/ECMAScriptHistoriaYVersionesjs/src/es8/02-string-padding.js
@@ -0,0 +1,18 @@
+const string = 'Hello'
+
+// console.log(string.padStart(7,'hi'));
+// console.log(string.padStart(8,'hi'));
+// console.log(string.padStart(10,'hi'));
+console.log(string.padStart(20,'_'));
+console.log('world'.padStart(20,'_'));
+console.log('test'.padStart(20,'_'));
+console.log('example'.padStart(20,'_'));
+
+
+// console.log(string.padEnd(7,'hi'));
+// console.log(string.padEnd(8,'hi'));
+// console.log(string.padEnd(10,'hi'));
+console.log(string.padEnd(20,'_'));
+console.log('world'.padEnd(20,'_'));
+console.log('test'.padEnd(20,'_'));
+console.log('example'.padEnd(20,'_'));
