caaf84d5c926b4b9338cec7fd7ec453393d5b195
Ospinajuanp <juanpo996@gmail.com>
2023-01-23 23:27:47 -0500

    Temple literal

---
+++ EscuelaPlatizWeb/ECMAScriptHistoriaYVersionesjs/src/es6/02-strings.js
@@ -0,0 +1,21 @@
+// Template literal 
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world + '!'
+
+console.log(epicPhrase);
+
+// Template literal -> comillas francesas ALT + (9)(6)
+
+let epicPhrase2 = `${hello}  ${world}!`
+
+console.log(epicPhrase2);
+
+let lorem = 'esto es un string \n' + 'esto es otra linea';
+
+let lorem2 = `esta es una frase épica
+esto es la continuación de la frase épica
+`;
+
+console.log(lorem);
+console.log(lorem2);
