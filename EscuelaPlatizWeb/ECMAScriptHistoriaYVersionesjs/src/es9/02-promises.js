1ac6f8ccc44a9f8d314864a1dfacd2c7087eaa79
Ospinajuanp <juanpo996@gmail.com>
2023-01-24 09:26:45 -0500

    Promises pero en es9

---
+++ EscuelaPlatizWeb/ECMAScriptHistoriaYVersionesjs/src/es9/02-promises.js
@@ -0,0 +1,15 @@
+const anotherFunction = () => {
+    return new Promise( (resolve,reject) => {
+        if(false){
+            resolve('Hola!')
+        }
+        else{
+            reject('Mal')
+        }
+    })
+}
+
+anotherFunction()
+    .then(response => console.log(response))
+    .catch(err => console.log(err))
+    .finally(() => console.log('Finally'))
